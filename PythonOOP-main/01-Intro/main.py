3e635fafe37c401dcf605788857227e956bf3640
Maroof Malik <146874434+maroofmalik1@users.noreply.github.com>
2024-07-12 11:26:27 -0700

    main.py

--- PythonOOP-main/01-Intro/main.py
+++ PythonOOP-main/01-Intro/main.py
@@ -1,9 +1,9 @@
-# How to create a class:
+
 class Item:
     def calculate_total_price(self, x, y):
         return x * y
 
-# How to create an instance of a class
+
 item1 = Item()
 
 # Assign attributes:
@@ -14,7 +14,6 @@ item1.quantity = 5
 # Calling methods from instances of a class:
 print(item1.calculate_total_price(item1.price, item1.quantity))
 
-# How to create an instance of a class (We could create as much as instances we'd like to)
 item2 = Item()
 
 # Assign attributes
@@ -22,5 +21,5 @@ item2.name = "Laptop"
 item2.price = 1000
 item2.quantity = 3
 
-# Calling methods from instances of a class: 
+
 print(item2.calculate_total_price(item2.price, item2.quantity))
